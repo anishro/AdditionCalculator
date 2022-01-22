ab5badb3c4eeb648746df53bd86431e4aefb8807
anishro <anishmanuudr@gmail.com>
2022-01-22 22:03:18 +0530

    incom

--- calculator.js
+++ calculator.js
@@ -14,7 +14,7 @@ app.post("/", function (req, res) {
   var num1 = Number(req.body.num1);
   var num2 = Number(req.body.num2);
   var result = num1 + num2;
-  res.send("The result of calculation is " + result);
+  res.send("result is " + result);
 });
 
 app.listen(3000, function () {
